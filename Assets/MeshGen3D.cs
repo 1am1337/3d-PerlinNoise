c8b44d3ffc6b645a2351c37e4d3f77bc45b0422c
1am1337 <oskar.nitzschker@icloud.com>
2026-06-03 15:48:41 +0200

    2d Perlin Noise (in 3D waow)

--- Assets/MeshGen3D.cs
+++ Assets/MeshGen3D.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 
@@ -8,12 +9,13 @@ public class MeshGen3D : MonoBehaviour
     Mesh mesh;
 
     Vector3[] vertices;
+    float[] verticesState;
     int[] triangles;
 
 
-    public int xSize = 20;
-    public int zSize = 20;
-    public int ySize = 20;
+    public int xSize = 10;
+    public int zSize = 10;
+    public int ySize = 10;
 
     public float strength = 0.3f;
 
@@ -30,7 +32,7 @@ public class MeshGen3D : MonoBehaviour
     void CreateShape()
     {
         vertices = new Vector3[(xSize + 1) * (ySize + 1) * (zSize + 1)];
-
+        verticesState = new float[vertices.Length];
         for (int i = 0, z = 0; z <= zSize; z++)
         {
             for (int x = 0; x <= xSize; x++)
@@ -38,10 +40,12 @@ public class MeshGen3D : MonoBehaviour
                 for (int y = 0; y <= ySize; y++)
                 {
                     vertices[i] = new Vector3(x, y, z);
+                    verticesState[i] = Mathf.PerlinNoise(x * strength, z * strength) * 2f;
                     i++;
                 }
             }
         }
+
     }
 
     void UpdateMesh()
@@ -62,7 +66,13 @@ public class MeshGen3D : MonoBehaviour
             return;
 
         }
-        for (int i = 0; i < vertices.Length; i++)
-            Gizmos.DrawSphere(vertices[i], .1f);
+        for (int i = 0; i < verticesState.Length; i++)
+        {
+            if (verticesState[i] > 0.8f)
+            {
+                Gizmos.color = new UnityEngine.Color32((byte)0, (byte)0, (byte)(verticesState[i] * 256), (byte)255);
+                Gizmos.DrawSphere(vertices[i], .1f);
+            }
+        }
     }
 }
