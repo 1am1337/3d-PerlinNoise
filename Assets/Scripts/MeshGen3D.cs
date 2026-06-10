6791d9eaa314dc941e666b8d8a2c31db194968a0
1am1337 <oskar.nitzschker@icloud.com>
2026-06-10 21:06:14 +0200

    started on marching cubes; moved scripts in proper folder

--- Assets/Scripts/MeshGen3D.cs
+++ Assets/Scripts/MeshGen3D.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 
 
+// Axes are:
+//
+//      y
+//      |     z
+//      |   /
+//      | /
+//      +----- x
+//
+// Vertex and edge layout:
+//
+//            6             7
+//            +-------------+               +-----6-------+
+//          / |           / |             / |            /|
+//        /   |         /   |          11   7         10  5
+//    2 +-----+-------+  3  |         +-----+2------+     |
+//      |   4 +-------+-----+ 5       |     +-----4-+-----+
+//      |   /         |   /           3   8         1   9
+//      | /           | /             | /           | /
+//    0 +-------------+ 1             +------0------+
+//
+//
+
 public class MeshGen3D : MonoBehaviour
 {
     Mesh mesh;
@@ -8,6 +30,7 @@ public class MeshGen3D : MonoBehaviour
     Vector3[] vertices;
     float[] verticesState;
     int[] triangles;
+    int[] cubePoints;
 
 
     public int xSize = 10;
@@ -18,11 +41,6 @@ public class MeshGen3D : MonoBehaviour
     public float noiseScale = 0.2f;
 
 
-    // public float freq = 2.0f;
-    // public float amp = 1.0f;
-    // public float persistence = 0.2f;
-    // public int oct = 3;
-    // public int seed = 0;
 
 
     void Start()
@@ -31,6 +49,7 @@ public class MeshGen3D : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
         CreateShape();
         //UpdateMesh();
+        GetEdges(0, 0, 0);
 
     }
 
@@ -50,11 +69,30 @@ public class MeshGen3D : MonoBehaviour
                     i++;
                 }
             }
-
         }
 
 
 
+
+    }
+
+    void GetEdges(int x, int y, int z)
+    {
+        cubePoints = new int[8];
+        cubePoints[0] = System.Array.IndexOf(vertices, new Vector3(x, y, z));
+        cubePoints[1] = System.Array.IndexOf(vertices, new Vector3(x + 1, y, z));
+        cubePoints[2] = System.Array.IndexOf(vertices, new Vector3(x, y + 1, z));
+        cubePoints[3] = System.Array.IndexOf(vertices, new Vector3(x + 1, y + 1, z));
+        cubePoints[4] = System.Array.IndexOf(vertices, new Vector3(x, y, z + 1));
+        cubePoints[5] = System.Array.IndexOf(vertices, new Vector3(x + 1, y, z + 1));
+        cubePoints[6] = System.Array.IndexOf(vertices, new Vector3(x, y + 1, z + 1));
+        cubePoints[7] = System.Array.IndexOf(vertices, new Vector3(x + 1, y + 1, z + 1));
+
+        for (int i = 0; i < cubePoints.Length; i++)
+        {
+            print(cubePoints[i]);
+        }
+        // return Edges;
     }
 
     void UpdateMesh()
